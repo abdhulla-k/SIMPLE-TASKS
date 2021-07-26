44c22a2a9022566e19336710a739e801021f62f5
abdhu-omy <abdhullak149@gmail.com>
2021-07-26 19:05:42 +0530

    ways for  string concatenation

---
+++ twelve_simple_projects/madlibs.py
@@ -0,0 +1,9 @@
+# string concatenation (how to put string together)
+
+youtouber = "freeCodeCamp"
+
+# a few ways to add this to 
+
+print("subscribe to " + youtouber)
+print("subscribe to {}".format(youtouber))
+print(f"subscribe to {youtouber}")
