68690c7328c300dd34aecaa61b5c5b4a9057e04c
abdhu-omy <abdhullak149@gmail.com>
2021-07-26 19:43:29 +0530

    self introduction demo creation.

--- twelve_simple_projects/madlibs.py
+++
@@ -1,9 +0,0 @@
-# string concatenation (how to put string together)
-
-youtouber = "freeCodeCamp"
-
-# a few ways to add this to 
-
-print("subscribe to " + youtouber)
-print("subscribe to {}".format(youtouber))
-print(f"subscribe to {youtouber}")
