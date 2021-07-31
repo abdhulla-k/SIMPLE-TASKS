ee911f0efcfbd5e40aa49a3ab5828f34f74c77e4
abdhulla-k <abdhullak149@gmail.com>
2021-07-31 20:25:48 +0530

    created board for tic tac toe

---
+++ simple_projects/tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,14 @@
+# going to create a design for tic tac toe
+
+def make_board(values):
+    print(' ____   ____   ____')
+    print('\n')
+    print('| {}  |  {}  |  {}  |'.format(values[0], values[1], values[2]))
+    print(' ____   ____   ____')
+    print('\n')
+    print('| {}  |  {}  |  {}  |'.format(values[3], values[4], values[5]))
+    print(' ____   ____   ____')
+    print('\n')
+    print('| {}  |  {}  |  {}  |'.format(values[6], values[7], values[8]))
+    print(' ____   ____   ____')
+    
