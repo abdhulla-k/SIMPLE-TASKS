d14d7cad2ff9a0c658dd2add3b62a5e415f93118
abdhulla-k <abdhullak149@gmail.com>
2021-07-31 21:30:13 +0530

    create datastructure and a try block for value error.

--- simple_projects/tic_tac_toe/tic_tac_toe.py
+++ simple_projects/tic_tac_toe/tic_tac_toe.py
@@ -11,4 +11,26 @@ def make_board(values):
     print('\n')
     print('| {}  |  {}  |  {}  |'.format(values[6], values[7], values[8]))
     print(' ____   ____   ____')
-    
+
+# values are below
+# want remember the past and prescent moves of player
+def move(cur_player):
+    values = [' ' for x in range(9)]
+    player_pse = {'x':[], 'o':[]}
+
+    while True:
+        make_board(values)
+        # exepting an error
+        # if a player enters unintended value
+        try:
+            print("player ", cur_player +"turn. which box? : ", end="")
+            move(int(input()))
+        # check the input. is it currect or not?
+        except ValueError:
+            print('wring input!! Try again')
+            continue
+        # check if the box is not sccupied olready
+        if move < 1 or move < 9:
+            print('wring input!!  Try again')
+            continue
+
