417fe84d731cb8f1890fdacde9c12a0b2dc69f68
abdhulla-k <abdhullak149@gmail.com>
2021-08-13 22:11:03 +0530

    created method avilable_moves

--- simple_projects/tic_tac_toe/Game.py
+++ simple_projects/tic_tac_toe/Game.py
@@ -10,3 +10,16 @@ def make_board():
 def print_board(self):
     for row in[self.board[i*3(i+1) *3]for i in range(3)]:
         print('| ' + ' | '.join(self.board) + ' |')
+
+@staticmethod
+def print_board_num():                             #print bord numbers
+    number_board = [[str(i)for i in range(j*3, (j+1)*3)for j in range(3)]]
+    for row in number_board:
+        print('| '+' | '.join(row)+' |')
+
+def avilable_moves(self):
+    moves = []
+    for (i, spot) in enumerate(self.board):
+        if spot == ' ':
+            moves.append(i)
+    return moves
