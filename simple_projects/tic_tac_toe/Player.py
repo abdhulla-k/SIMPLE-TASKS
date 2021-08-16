2cdc0396a324c3a5ecd5d6fbceb7b33ea64c977f
abdhulla-k <abdhullak149@gmail.com>
2021-08-16 23:44:42 +0530

    created function 'get_move()'.to give next move for players'

--- simple_projects/tic_tac_toe/Player.py
+++ simple_projects/tic_tac_toe/Player.py
@@ -7,16 +7,28 @@ class player:
 
     def get_move(self):
         pass
-class HuaanPlayer(player):
+class HumanPlayer(player):
     def __init__(self, letter):
         super().__init__(letter)
     
-    def get_move(self):
-        pass
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move(0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('invalid square. Try again')
+            return val
 
 class RandomComputerPlayer(player):
     def __init__(self, letter):
         super().__init__(letter)
     
-    def get_move(self):
-        pass
+    def get_move(self, game):
+        square = random.choice(game.avilable_moves())
+        return square
