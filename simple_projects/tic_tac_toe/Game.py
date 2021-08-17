6035d06a74122cedc84bd4455cb87432ed4683ba
abdhulla-k <abdhullak149@gmail.com>
2021-08-17 17:44:49 +0530

    completed prlject. Created Smart computer project .

--- simple_projects/tic_tac_toe/Game.py
+++ simple_projects/tic_tac_toe/Game.py
@@ -1,94 +1,101 @@
 import math
 import time
-from Player import HumanPlayer, RandomComputerPlayer
-class TicTacToe:                         # going to create game
-    def __init__(self):                  # need a board for play.
+from player import HumanPlayer, SmartComputerPlayer
+class TicTacToe():
+    def __init__(self):
         self.board = self.make_board()
-        self.curret_winner = None         # keep track of winner.
+        self.current_winner = None
 
     @staticmethod
     def make_board():
-        return [' ' for _  in range(9)]
-    
+        return [' ' for _ in range(9)]
+
     def print_board(self):
-        for row in[self.board[i*3(i+1) *3]for i in range(3)]:
-            print('| ' + ' | '.join(self.board) + ' |')
-    
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
     @staticmethod
-    def print_board_num():                             #print bord numbers
-        number_board = [[str(i)for i in range(j*3, (j+1)*3)for j in range(3)]]
+    def print_board_nums():
+        # 0 | 1 | 2
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
-            print('| '+' | '.join(row)+ ' |')
-    
-    def avilable_moves(self):
-        moves = []
-        for (i, spot) in enumerate(self.board):
-            if spot == ' ':
-                moves.append(i)
-        return moves 
-    def empty_squares(self):
-        return self.board
-    def num_empty_squares(self):
-        return self.board.count(' ')
-    
+            print('| ' + ' | '.join(row) + ' |')
+
     def make_move(self, square, letter):
         if self.board[square] == ' ':
             self.board[square] = letter
-            if self.winner(square,letter):
-                self.curret_winner = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
             return True
         return False
-    def winner(self, square, letter):
-        # check all possibilities
 
-        #check the row
+    def winner(self, square, letter):
+        #lets chck the winner
+        # check the row
         row_ind = math.floor(square / 3)
         row = self.board[row_ind*3:(row_ind+1)*3]
+        # print('row', row)
         if all([s == letter for s in row]):
             return True
-
-        #check the column
         col_ind = square % 3
         column = [self.board[col_ind+i*3] for i in range(3)]
+        
         if all([s == letter for s in column]):
             return True
-
-        #check the digonal
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            
             if all([s == letter for s in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            
             if all([s == letter for s in diagonal2]):
                 return True
         return False
 
-def play(game, x_player, o_player, print_game = True):
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def available_moves(self):
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
+def play(game, x_player, o_player, print_game=True):
+
     if print_game:
-        game.empty_squares()
-    
+        game.print_board_nums()
+
     letter = 'X'
     while game.empty_squares():
         if letter == 'O':
-            square = o_player.getmove(game)
+            square = o_player.get_move(game)
         else:
-            square = x_player.getmove(game)
-        # define make move. when we make a move, we need information about what square the user wats their move to be at.
-        # and what letter the player is?
+            square = x_player.get_move(game)
         if game.make_move(square, letter):
-            
+
             if print_game:
-                print(letter + f'makes a move to square {square}')
+                print(letter + ' makes a move to square {}'.format(square))
                 game.print_board()
-                print('') # an empty line
-                # find winner
+                print('')
+
             if game.current_winner:
                 if print_game:
-                    print(letter + "winns !")
-                    return letter  # ends the loop and exits the game.
-            letter = 'O' if letter == 'X' else 'X' # switches player
+                    print(letter + ' wins!')
+                return letter  # ends the loop and exits the game
+            letter = 'O' if letter == 'X' else 'X'  # switches player
 
         time.sleep(.8)
+
     if print_game:
-        print("It\'s a tie")
-    
+        print('It\'s a tie!')
+
+
+
+if __name__ == '__main__':
+    x_player = SmartComputerPlayer('X')
+    o_player = HumanPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
