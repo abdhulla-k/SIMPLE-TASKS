99f84f058bd029f3df550356873682282ac4d92d
abdhulla-k <abdhullak149@gmail.com>
2021-08-17 16:12:27 +0530

    created empty_'squares' and 'num_empty_squares'. Started on play method

--- simple_projects/tic_tac_toe/Game.py
+++ simple_projects/tic_tac_toe/Game.py
@@ -3,23 +3,39 @@ class TicTacToe:                         # going to create game
         self.board = self.make_board()
         self.curret_winner = None         # keep track of winner.
 
-@staticmethod
-def make_board():
-    return [' ' for _  in range(9)]
+    @staticmethod
+    def make_board():
+        return [' ' for _  in range(9)]
+    
+    def print_board(self):
+        for row in[self.board[i*3(i+1) *3]for i in range(3)]:
+            print('| ' + ' | '.join(self.board) + ' |')
+    
+    @staticmethod
+    def print_board_num():                             #print bord numbers
+        number_board = [[str(i)for i in range(j*3, (j+1)*3)for j in range(3)]]
+        for row in number_board:
+            print('| '+' | '.join(row)+ ' |')
+    
+    def avilable_moves(self):
+        moves = []
+        for (i, spot) in enumerate(self.board):
+            if spot == ' ':
+                moves.append(i)
+        return moves 
+    def empty_squares(self):
+        return self.board
+    def num_empty_squares(self):
+        return self.board.count(' ')
 
-def print_board(self):
-    for row in[self.board[i*3(i+1) *3]for i in range(3)]:
-        print('| ' + ' | '.join(self.board) + ' |')
-
-@staticmethod
-def print_board_num():                             #print bord numbers
-    number_board = [[str(i)for i in range(j*3, (j+1)*3)for j in range(3)]]
-    for row in number_board:
-        print('| '+' | '.join(row)+' |')
-
-def avilable_moves(self):
-    moves = []
-    for (i, spot) in enumerate(self.board):
-        if spot == ' ':
-            moves.append(i)
-    return moves
+def play(game, x_player, o_player, print_game = True):
+    if print_game:
+        game.empty_squares()
+    
+    letter = 'X'
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.getmove(game)
+        else:
+            square = x_player.getmove(game)
+        pass
