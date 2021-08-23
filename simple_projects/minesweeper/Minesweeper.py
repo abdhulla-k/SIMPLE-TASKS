87296d65b30e7c1206e8585fc54a6a2093d0e73f
abdhulla-k <abdhullak149@gmail.com>
2021-08-23 19:25:15 +0530

    started with object oriented programing. created class Board

---
+++ simple_projects/minesweeper/Minesweeper.py
@@ -0,0 +1,11 @@
+
+
+# lets create a board object to represent the minesweeper game
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+
+def Play(dim_size = 10, num_bombs = 10):
+    pass
