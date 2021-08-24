4b5e588b8992491a15f875db488c14b3c96990ca
abdhulla-k <abdhullak149@gmail.com>
2021-08-24 20:19:30 +0530

    created multiplication_table.py.then created loop and get text for speek

---
+++ simple_projects/test_reader/multiplication_table.py
@@ -0,0 +1,19 @@
+n = input("Enter a number")
+
+dic = {
+    "1" : "ones",
+    "2" : "twos",
+    "3" : "threes",
+    "4" : "fours",
+    "5" : "fives",
+    "6" : "sixes",
+    "7" : "sevens",
+    "8" : "eights",
+    "9" : "nines",
+    "10": "tens"
+}
+current_num = ""
+for i in range(1,11):
+    mult = i*int(n)
+    current_num = str(i) + " " +dic[n] + " are " + str(mult) +"\n"
+    print(current_num)
