891f8a79cbe4d84c943bae058160c076349bd40d
abdhulla-k <abdhullak149@gmail.com>
2021-08-24 19:12:23 +0530

    created string function which will print the visivle board for users.

--- simple_projects/minesweeper/Minesweeper.py
+++ simple_projects/minesweeper/Minesweeper.py
@@ -84,8 +84,55 @@ class Board:
         
         return True
     
-    
+    def __str__(self):
+        # firs let's crate a new array that represent what the user would see
+        visible_board = [[None for i in range(self.dim_size)] for None in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row,col) in self.dug:
+                     visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+        
+        # put this together in a string
+        string_rep = ''
+        # get max column widths for printing
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
 
+        # print the csv strings
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
+                
+
+            
 
 def Play(dim_size = 10, num_bombs = 10):
     #create the board and plant the bombs
