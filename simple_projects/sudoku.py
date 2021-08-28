c60c20ae33e9db23ae726c090d08e1f58981190b
abdhulla-k <abdhullak149@gmail.com>
2021-08-28 21:27:35 +0530

    created method 'is_valid' which will figures out whether the guess at the row/col of the puzzle is valid or not

--- simple_projects/sudoku.py
+++ simple_projects/sudoku.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def find_next_empty(puzzle):
     # find next row and col on the puzzle that's not filled yet --> rep with -1
     # return row, col = non, non if there is not an empty space
@@ -8,7 +11,46 @@ def find_next_empty(puzzle):
     
     return None, None # there is not an empty space
 
+def is_valid(puzzle, guess, row, col):
+    # figures out whether the guess at the row/col of the puzzle is valid or guess
+    # return True if is valid, False otherwise
+
+    # incase of row
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+    
+    # incase of col
+
+    # col_val = []
+    # for i in range(9):
+    #    col_val.append(puzzle[i][col]).  an another way is used below
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+    
+    # incase of square
+    # figures out where the 3x3 square starts
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+    # iterate over the 3 values in the row/column
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if guess == puzzle[r][c]:
+                return False
+    
+    return True
+
 def solve_sudoku(puzzle):
     # solve sudoku using backtracking
     # step 1: choose somewhere on the puzzle to make a guess.
     row, col = find_next_empty(puzzle)
+
+    # if there's nowhere left
+    if row == None:
+        return True # becouse we've actually solved our puzle.
+    
+    # if there is a place to put a number, then make a guess between 1 and 9
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
+
