f6d4f4bcf729fac059d7cb26a22e616528afccf9
abdhulla-k <abdhullak149@gmail.com>
2021-08-31 21:43:41 +0530

    started a basic banking project. created Base Class

---
+++ simple_projects/Bank/Bank.py
@@ -0,0 +1,12 @@
+class BaseClass:
+    AccountBalance = 0
+    def __init__(self,username, password, name, place, age, gender, balance):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.place = place
+        self.age = age
+        self.gender = gender
+        self.balance = balance
+
+    
