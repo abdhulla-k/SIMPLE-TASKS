ad4915ed8d11cf6a5912f0a2a623b48fa55d0fec
abdhulla-k <abdhullak149@gmail.com>
2021-09-01 18:35:51 +0530

    created login mithod

--- simple_projects/Bank/Bank.py
+++ simple_projects/Bank/Bank.py
@@ -1,12 +1,22 @@
-class BaseClass:
-    AccountBalance = 0
-    def __init__(self,username, password, name, place, age, gender, balance):
-        self.username = username
-        self.password = password
-        self.name = name
-        self.place = place
-        self.age = age
-        self.gender = gender
-        self.balance = balance
+class main:
+    UserName = 'Abdhu'
+    Password = 'abcdefgh'
 
-    
+    def Login(self):
+        UserName = input('Enter'+'\n'+'username: ')
+        Password = input('Password: ')
+        if UserName == self.UserName and Password == self.Password:
+                print('Welcome to Bank'+'\n'+'Login completed')
+        else:
+            print('wrong Username or Password')
+        
+
+def Bank(Userinpu):
+    print(UserInput)
+    x = main()
+    x.Login()
+
+
+if __name__=='__main__':
+    UserInput = input('Enter'+'\n'+'1 for Login')
+    Bank(UserInput)
