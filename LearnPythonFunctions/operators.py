36a3801afde5284c2f9a889fdaa4f877646c6197
abdhulla-k <abdhullak149@gmail.com>
2022-03-13 18:49:17 +0530

    learned the opearators in python

---
+++ LearnPythonFunctions/operators.py
@@ -0,0 +1,53 @@
+from tkinter import Y
+
+
+a = 2
+b = 3
+c = 5
+
+# + addition 
+print(a+b)  # a+b = 2+3 = 5
+
+# - substraction
+print(c-a)  # c-a = 5-2 = 3
+
+# * multiplication 
+print(a*b)  # a*b = 2*3 = 6
+
+# / division 
+print(c/a)  # c/a = 5/2 = 2
+
+# % modulus 
+print(c%a) # c/a = 5/2 = 1
+
+# ** Exponentiation
+print(a*c) # a*c = 2*2*2*2*2 = 32
+
+# // floor division
+print(c//a) # c//a = 5//2 = 2
+
+# =	x = 5	x = 5
+# 	
+# +=	x += 3	x = x + 3
+
+# -=	x -= 3	x = x - 3
+
+# *=	x *= 3	x = x * 3
+# 	
+# /=	x /= 3	x = x / 3
+# 	
+# %=	x %= 3	x = x % 3
+# 	
+# //=	x //= 3	x = x // 3
+# 	
+# **=	x **= 3	x = x ** 3
+# 	
+# &=	x &= 3	x = x & 3
+# 	
+# |=	x |= 3	x = x | 3
+# 	
+# ^=	x ^= 3	x = x ^ 3
+# 	
+# >>=	x >>= 3	x = x >> 3
+# 	
+# <<=	x <<= 3	x = x << 3
