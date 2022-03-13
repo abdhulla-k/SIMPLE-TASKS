c5f442bef39478f7cb5e881cacd00ed13058cce0
abdhulla-k <abdhullak149@gmail.com>
2022-03-13 17:34:06 +0530

    learnd the base of format function

---
+++ LearnPythonFunctions/FormatFunction.py
@@ -0,0 +1,19 @@
+# format function
+
+# 1
+age = 21
+text = 'my age is {}'
+print(text.format(age))
+
+# 2
+Class = 12
+phoneNumber = 22222222
+text = 'I am {} years old. I have completed my {}th standard. my phone number is {}'
+print(text.format(age, Class, phoneNumber))
+
+#3
+Class = 10
+age = 22
+phoneNumber = 3333333333333
+test = 'I am {2} years old. I have completed my {1}th standard. my phone number is {0}'
+print(text.format(age, Class, phoneNumber))
